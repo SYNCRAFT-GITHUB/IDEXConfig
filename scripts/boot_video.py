0526057b3e9848c1dbee76e5b403baeace032ba5
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2024-12-04 15:59:46 -0300

    Don't block process when playing video

--- scripts/boot_video.py
+++ scripts/boot_video.py
@@ -1,8 +1,9 @@
 import os
+import subprocess
 from constants import *
 
 def play_video(path: str):
-	return os.system(f"sudo -u pi cvlc -f --no-video-title-show {path} 2> /dev/null")
+	return subprocess.run(["sudo", "-u", "pi", "cvlc", "-f", "--no-video-title-show", path])
 
 def play_boot_video():
 	play_video(os.path.join(IDEXCONFIG_PATH, "boot_videos", "default.mp4"))
