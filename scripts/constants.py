209ab4c7987adbd71e12b381af9b26080150aa92
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2024-12-04 14:24:27 -0300

    fix IDEXConfig location

--- scripts/constants.py
+++ scripts/constants.py
@@ -2,7 +2,7 @@ import os
 
 HOME = os.path.join("/home", "pi")
 
-IDEXCONFIG_PATH = os.path.join(HOME, "IDEXConfig")
+IDEXCONFIG_PATH = os.path.join(HOME, "printer_data", "config")
 PRINTER_CFG_PATH = os.path.join(HOME, "printer_data", "config", "printer.cfg")
 
 BACKUP_PRINTER_CFG_PATH = os.path.join(IDEXCONFIG_PATH, "backups", "printer.cfg")
