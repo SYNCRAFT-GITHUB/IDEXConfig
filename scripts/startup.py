99f18339b57bafe350419fe1acbfc0da6f6a7d8b
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-01-08 10:14:08 -0300

    Copy variables in startup if doesn't exist

--- scripts/startup.py
+++ scripts/startup.py
@@ -1,5 +1,12 @@
+import os
+import shutil
 from transfer import update_printer_config
 from boot_video import play_boot_video
+from constants import *
 
 update_printer_config()
+
+if not os.path.exists(VARIABLES_PATH):
+	shutil.copyfile(BACKUP_VARIABLES_PATH, VARIABLES_PATH)	
+
 play_boot_video()
