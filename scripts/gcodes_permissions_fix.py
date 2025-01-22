96bbc2951b449c951d27f2eb21682c133baf792e
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-01-22 17:25:55 -0300

    Fix subprocess function call argument

--- scripts/gcodes_permissions_fix.py
+++ scripts/gcodes_permissions_fix.py
@@ -2,7 +2,7 @@ import subprocess
 
 def fix_gcodes_permissions():
 	try:
-		subprocess.run("sudo chown pi /home/pi/printer_data/gcodes")
-		subprocess.run("sudo chown pi /home/pi/printer_data/gcodes/.JOB")
+		subprocess.run(["sudo", "chown", "pi", "/home/pi/printer_data/gcodes"])
+		subprocess.run(["sudo", "chown", "pi", "/home/pi/printer_data/gcodes/.JOB"])
 	except:
 		pass
